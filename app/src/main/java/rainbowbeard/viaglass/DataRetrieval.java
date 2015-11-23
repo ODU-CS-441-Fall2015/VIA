f34b15cf2eed7ef2e49a2c99fad3dc8e83ee7b8d
Matt Chaney <mchaney@alionscience.com>
2015-11-22 19:24:59 -0500

    Adding parsing reverse image search response for test URL.

---
+++ app/src/main/java/rainbowbeard/viaglass/DataRetrieval.java
@@ -0,0 +1,22 @@
+package rainbowbeard.viaglass;
+
+/**
+ * Created by Bliss on 11/10/2015.
+ Wiki retriever: (class)
+ Receive name
+ Search for name
+ Filter for whitelist domains
+ If no
+ Send no data available
+ Send available information to developer
+ If yes
+ Send address => Information extractor
+ Information extractor: (class)
+ Receive address
+ Parse site
+ Extract data
+ Send data => Data display
+ */
+public class DataRetrieval {
+
+}
--- app/src/main/java/rainbowbeard/viaglass/ImageSearchActivity.java
+++ app/src/main/java/rainbowbeard/viaglass/ImageSearchActivity.java
@@ -1,74 +1,49 @@
 package rainbowbeard.viaglass;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 public class ImageSearchActivity extends AppCompatActivity {
     private static final String TAG = ImageSearchActivity.class.getCanonicalName();
-    private static final String
-            URIHEAD = "https://images.google.com/searchbyimage?image_url=http%3A%2F%2Fwww.cs.odu.edu%2F%7Emchaney%2Fuploads%2F",
-            URITAIL = "&image_content=&filename=&hl=en";
-    private ProgressDialog mProgressDialog;
+    public static final String SEARCH_RESPONSE_EVENT = "IMAGE_SEARCH_RESPONSE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "created activity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagesearch);
-    }
 
-    public void onRetrieveClick(View v) {
-        new TestRetriever(encodeURI("upload-1")).execute();
-    }
+        // create our search response event broadcast receiver
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, "received " + SEARCH_RESPONSE_EVENT + " broadcast");
 
-    private static String encodeURI(final String filename) {
-        return URIHEAD + filename + URITAIL;
-    }
-
-    private class TestRetriever extends AsyncTask<Void, Void, Void> {
-        private final String url;
-        String response;
-
-        public TestRetriever(final String url) {
-            this.url = url;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mProgressDialog = new ProgressDialog(ImageSearchActivity.this);
-            mProgressDialog.setTitle("Android Basic JSoup Tutorial");
-            mProgressDialog.setMessage("Loading...");
-            mProgressDialog.setIndeterminate(false);
-            mProgressDialog.show();
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                Document document = Jsoup.connect(url).followRedirects(true).get();
-                Elements elements = document.select("._gUb");
-                elements.text();
-//                response =
-            } catch (Exception e) {
-                Log.e(TAG, e.getMessage());
+                // extract the text from the response event and display it
+                final String response = intent.getStringExtra(SEARCH_RESPONSE_EVENT);
+                if(null != response) {
+                    final TextView resultView = (TextView) findViewById(R.id.result_view);
+                    resultView.setText(response);
+                }
             }
-            return null;
-        }
+        }, new IntentFilter(SEARCH_RESPONSE_EVENT));
+        Log.d(TAG, "registered broadcast receiver for " + SEARCH_RESPONSE_EVENT);
+    }
 
-        @Override
-        protected void onPostExecute(Void result) {
-            Toast toast = Toast.makeText(ImageSearchActivity.this, response, Toast.LENGTH_LONG);
-            toast.show();
-            mProgressDialog.dismiss();
-        }
+    public void onRetrieveClick(final View v) {
+        Log.d(TAG, "retrieve clicked");
+        final String imageFile = "uploads/upload-1.jpg";
+        new ImageSearchTask(this, imageFile).start();
+        final Toast toast = Toast.makeText(this, "Searching for image: " + imageFile, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
