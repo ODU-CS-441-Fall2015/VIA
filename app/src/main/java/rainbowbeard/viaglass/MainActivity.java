ffce198e973a8d1ef8c7e26369cbc990ff517b72
Chris Bliss <cblis001@odu.edu>
2015-11-26 19:30:38 -0500

    lost camera view, camera launches from main activity

--- app/src/main/java/rainbowbeard/viaglass/MainActivity.java
+++ app/src/main/java/rainbowbeard/viaglass/MainActivity.java
@@ -1,12 +1,23 @@
 package rainbowbeard.viaglass;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getCanonicalName();
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +26,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCameraClick(View v) {
-        Intent intent = new Intent(this, CameraActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, CameraActivity.class);
+        //startActivity(intent);
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStoragePublicDirectory
+                (Environment.DIRECTORY_PICTURES), "picture.jpg");
+        imageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, 1); //1 for rear-facing camera
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if(resultCode== Activity.RESULT_OK){
+            Uri selectedImage=imageUri;
+            getContentResolver().notifyChange(selectedImage,null);
+
+            ImageView imageView = (ImageView)findViewById(R.id.image_camera);
+            ContentResolver cr =getContentResolver();
+            Bitmap bitmap;
+
+
+            try{
+                bitmap = MediaStore.Images.Media.getBitmap(cr,selectedImage);
+                imageView.setImageBitmap(bitmap);
+            }catch(Exception e){
+                Log.e("Image retrieve fail:", e.toString());
+            }
+        }
     }
 
     public void onRetrieverClick(View view) {
